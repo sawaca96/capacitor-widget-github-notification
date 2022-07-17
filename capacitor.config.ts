83cb258fd4eccb6835a7b07f96e8ffc0c4e6e7b1
sawaca96 <sawaca96@gmail.com>
2022-07-17 16:50:07 +0900

    🎉 Start widget-practice

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'widget-practice',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
