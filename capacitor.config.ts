a5b7b6f4429cea49b26a8a0d1eb71ffae5a6fae0
sawaca96 <sawaca96@gmail.com>
2022-09-22 00:37:51 +0900

    ✨ Web Github Login

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,19 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'widget-practice',
-  webDir: 'dist',
-  bundledWebRuntime: false
+  appId: "io.ionic.starter",
+  appName: "widget-practice",
+  webDir: "dist",
+  bundledWebRuntime: false,
+  server: {
+    url: "http://192.168.0.161:8080/",
+    cleartext: true,
+  },
+  plugins: {
+    FirebaseAuthentication: {
+      providers: ["github.com"],
+    },
+  },
 };
 
 export default config;
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,77 @@
 <template>
-  <ion-app>
-    <ion-router-outlet />
+  <ion-app class="ion-align-items-center ion-justify-content-center">
+    <ion-button
+      color="success"
+      v-if="githubToken === ''"
+      @click="signInWithGithub"
+      >SAVE TOKEN</ion-button
+    >
+    <ion-button color="danger" v-else @click="signOut">REMOVE TOKEN</ion-button>
   </ion-app>
 </template>
 
-<script lang="ts">
-import { IonApp, IonRouterOutlet } from '@ionic/vue';
-import { defineComponent } from 'vue';
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import { IonButton, IonApp, alertController } from "@ionic/vue";
+import { FirebaseAuthentication } from "@capacitor-firebase/authentication";
+import { Capacitor } from "@capacitor/core";
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    IonApp,
-    IonRouterOutlet
-  }
+const email = ref("");
+const photoUrl = ref("");
+const githubToken = ref("");
+
+onMounted(async () => {
+  githubToken.value = (await getIdToken()) || "";
 });
+
+const getIdToken = async () => {
+  if (Capacitor.isNativePlatform()) {
+    return "";
+  } else {
+    return localStorage.getItem("githubToken");
+  }
+};
+
+const signInWithGithub = async () => {
+  const result = await FirebaseAuthentication.signInWithGithub();
+  email.value = result?.user?.email as string;
+  photoUrl.value = result?.user?.photoUrl as string;
+  saveToken(result?.credential?.accessToken as string);
+  presentAlert("success sign in");
+  return result.user;
+};
+
+const saveToken = async (token: string) => {
+  githubToken.value = token;
+  if (Capacitor.isNativePlatform()) {
+    return;
+  } else {
+    localStorage.setItem("githubToken", token);
+  }
+};
+
+const signOut = async () => {
+  await FirebaseAuthentication.signOut();
+  removeToken();
+  presentAlert("success sign out");
+};
+
+const removeToken = async () => {
+  githubToken.value = "";
+  if (Capacitor.isNativePlatform()) {
+    return;
+  } else {
+    localStorage.removeItem("githubToken");
+  }
+};
+
+const presentAlert = async (message: string) => {
+  const alert = await alertController.create({
+    header: "INFO",
+    message: message,
+    buttons: ["OK"],
+  });
+
+  await alert.present();
+};
 </script>
