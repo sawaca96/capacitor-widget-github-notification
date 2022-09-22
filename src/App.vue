58899c8a1690418d322b1da97bfbda385d547891
sawaca96 <sawaca96@gmail.com>
2022-09-23 00:40:37 +0900

    💄 Add avatar and email label

--- src/App.vue
+++ src/App.vue
@@ -2,55 +2,114 @@
   <ion-app class="ion-align-items-center ion-justify-content-center">
     <ion-button
       color="success"
-      v-if="githubToken === ''"
+      v-if="status === 'SIGN_OUT'"
       @click="signInWithGithub"
       >SAVE TOKEN</ion-button
     >
-    <ion-button color="danger" v-else @click="signOut">REMOVE TOKEN</ion-button>
+    <div v-else-if="status === 'SIGN_IN'" style="display: contents">
+      <ion-chip>
+        <ion-avatar class="ion-justify-content-center">
+          <img alt="github user image" :src="photoUrl" />
+        </ion-avatar>
+        <ion-label>{{ email }}</ion-label>
+      </ion-chip>
+      <ion-button color="danger" @click="signOut">REMOVE TOKEN</ion-button>
+    </div>
+    <ion-spinner v-else type="medium"></ion-spinner>
   </ion-app>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
-import { IonButton, IonApp, alertController } from "@ionic/vue";
-import { FirebaseAuthentication } from "@capacitor-firebase/authentication";
+import {
+  IonButton,
+  IonApp,
+  alertController,
+  IonAvatar,
+  IonSpinner,
+  IonChip,
+  IonLabel,
+} from "@ionic/vue";
+import {
+  FirebaseAuthentication,
+  SignInResult,
+} from "@capacitor-firebase/authentication";
 import { SecureStoragePlugin } from "capacitor-secure-storage-plugin";
 
-const email = ref("");
 const photoUrl = ref("");
-const githubToken = ref("");
+const email = ref("");
+const token = ref("");
+const status = ref("PROGRESS");
 
 onMounted(async () => {
-  githubToken.value = (await getToken()) || "";
+  initValues();
 });
 
-const getToken = async () => {
-  return (await SecureStoragePlugin.get({ key: "githubToken" })).value;
+const initValues = async () => {
+  await SecureStoragePlugin.get({ key: "githubToken" })
+    .then((result) => {
+      token.value = result.value;
+      status.value = "SIGN_IN";
+    })
+    .catch(() => {
+      status.value = "SIGN_OUT";
+    });
+  await SecureStoragePlugin.get({ key: "photoUrl" })
+    .then((result) => {
+      photoUrl.value = result.value;
+    })
+    .catch(() => {
+      return;
+    });
+  await SecureStoragePlugin.get({ key: "email" })
+    .then((result) => {
+      email.value = result.value;
+    })
+    .catch(() => {
+      return;
+    });
 };
 
 const signInWithGithub = async () => {
   const result = await FirebaseAuthentication.signInWithGithub();
-  email.value = result?.user?.email as string;
-  photoUrl.value = result?.user?.photoUrl as string;
-  saveToken(result?.credential?.accessToken as string);
-  presentAlert("success sign in");
+  await presentAlert("success sign in");
+  saveUserInfo(result);
+  status.value = "SIGN_IN";
   return result.user;
 };
 
-const saveToken = async (token: string) => {
-  githubToken.value = token;
-  SecureStoragePlugin.set({ key: "githubToken", value: token });
+const saveUserInfo = async (result: SignInResult) => {
+  token.value = result?.credential?.accessToken as string;
+  photoUrl.value = result?.user?.photoUrl as string;
+  email.value = result?.user?.email as string;
+  SecureStoragePlugin.set({
+    key: "githubToken",
+    value: token.value,
+  });
+  SecureStoragePlugin.set({
+    key: "photoUrl",
+    value: photoUrl.value,
+  });
+  SecureStoragePlugin.set({
+    key: "email",
+    value: email.value,
+  });
 };
 
 const signOut = async () => {
   await FirebaseAuthentication.signOut();
-  removeToken();
-  presentAlert("success sign out");
+  await presentAlert("success sign out");
+  removeUserInfo();
+  status.value = "SIGN_OUT";
 };
 
-const removeToken = async () => {
-  githubToken.value = "";
+const removeUserInfo = async () => {
+  token.value = "";
+  photoUrl.value = "";
+  email.value = "";
   SecureStoragePlugin.remove({ key: "githubToken" });
+  SecureStoragePlugin.remove({ key: "photoUrl" });
+  SecureStoragePlugin.remove({ key: "email" });
 };
 
 const presentAlert = async (message: string) => {
@@ -59,7 +118,6 @@ const presentAlert = async (message: string) => {
     message: message,
     buttons: ["OK"],
   });
-
   await alert.present();
 };
 </script>
