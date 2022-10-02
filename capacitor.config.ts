d13efa60fe6d769745289b0dcecf25cf42822a79
sawaca96 <sawaca96@gmail.com>
2022-10-02 22:03:29 +0900

    🎉 Start ionic project

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-widget-github-notification',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
